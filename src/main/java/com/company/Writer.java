901b9a2ddbf8c89a0091413acb6b150ebc528e14
Paul Korver <prkorver@hotmail.com>
2019-02-11 11:43:27 +0100

    Added WriterTest and adjusted method for writer to handle empty inputs.

--- src/main/java/com/company/Writer.java
+++ src/main/java/com/company/Writer.java
@@ -9,18 +9,49 @@ public class Writer {
 
     public void writePerson(Person p) {
 
+        String personName;
+        // Only want to add space between names if both are provided
+        if(!p.getFirstName().equals("") && !p.getLastName().equals("")){
+            personName = p.getFirstName()  + " " + p.getLastName();
+        } else {
+            //If on of the names is provided the format will be okay, if none of the names is provided will become ""
+            personName = p.getFirstName() + p.getLastName();
+        }
+        String personUserName = p.getUserName();
+        String personOrgName = p.getOrg().getName();
+        String personExtension = (p.getExtensionNumber() == null) ? "" : p.getExtensionNumber().toString();
+        String [] personDescription = { personName, personUserName, personOrgName, personExtension };
 
-        System.out.println("Person with name " + p.getFirstName() + " " + p.getLastName() +
-                           " has username: " + p.getUserName() + " who works for " + p.getOrg() +
-                           " and gets extension number: " + p.getExtensionNumber());
+        for(int i = 0; i < personDescription.length; i++) {
+            if( personDescription[i].equals("")) {
+                personDescription[i] = "not provided";
+            }
+        }
+
+
+        System.out.println("Person with name " + personDescription[0] +
+                           " has username: " + personDescription[1] + " who works for " + personDescription[2] +
+                           " and gets extension number: " + personDescription[3]);
 
     }
 
     public void writeOrganization(Organization o) {
 
+        String organizationName = o.getName();
+        String organizationEmail = o.getEmail();
+        String organizationPhoneNumber = o.getPhoneNumber();
+        String[] organizationDescrption = { organizationName, organizationEmail, organizationPhoneNumber };
 
-        System.out.println("Organization " + o.getName() + " is reachable on email at: " +
-                           o.getEmail() + " and through phone by: " + o.getPhoneNumber() + ".");
+        for(int i = 0; i < organizationDescrption.length; i++) {
+            if( organizationDescrption[i].equals("")) {
+                organizationDescrption[i] = "not provided";
+            }
+        }
+
+
+
+        System.out.println("Organization " + organizationDescrption[0] + " is reachable on email at: " +
+                organizationDescrption[1] + " and through phone by: " + organizationDescrption[2] + ".");
     }
 
     public void write(List list) {
