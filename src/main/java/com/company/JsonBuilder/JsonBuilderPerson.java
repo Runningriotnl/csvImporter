d488021a6fd65b01596206b8073e56a59522b6ae
Paul Korver <prkorver@hotmail.com>
2019-02-15 10:50:42 +0100

    Added method to create organisation and telecomaddress in jsonString.

--- src/main/java/com/company/JsonBuilder/JsonBuilderPerson.java
+++ src/main/java/com/company/JsonBuilder/JsonBuilderPerson.java
@@ -4,9 +4,9 @@ import com.company.MakeHttpRequest;
 import com.company.person.Person;
 
 public class JsonBuilderPerson {
-    String personAsJson;
-    Person person;
-    String organizationOID;
+    private String personAsJson;
+    private Person person;
+    private String organizationOID;
 
     public JsonBuilderPerson(Person person) {
         this.person = person;
@@ -19,12 +19,57 @@ public class JsonBuilderPerson {
     }
 
     public String createOrganisationBlock() {
-        if(!"".equals(person.getOrg().getName())) {
+        String employment;
+        String personOrgName = person.getOrg().getName();
+        if(!"".equals(personOrgName)) {
             MakeHttpRequest makeHttpRequest = new MakeHttpRequest("http://10.78.40.157");
             organizationOID = makeHttpRequest.getOrganizationOID(person.getOrg().getName());
-
+            employment = "\"employments\": [" +
+                    "{\"commonName\":\"" + personOrgName + "\"," +
+                    "\"organisation\":{" +
+                    "\"commonName\":\"" + personOrgName + "\"," +
+                    "\"oid\":" + organizationOID + "\"," +
+                    "\"objectType\":\"Organisation\"}," +
+                    "\"telecomAddresses\":" + createTelecomAddressesBlock() +
+                    "}]";
+        } else {
+            employment = "\"employments\": []";
         }
-        return organizationOID;
+
+        return employment;
+    }
+
+    public String  createTelecomAddressesBlock() {
+        String telecomAddresses;
+        String personOrgEmail = person.getEmail();
+        String personOrgTel = person.getMobileNumber();
+
+        if(!"".equals(personOrgEmail) && !"".equals(personOrgTel)) {
+            telecomAddresses = "[{" +
+                               "\"address\":\"" + personOrgEmail + "\"," +
+                               "\"addressType\":\"Email\"," +
+                               "\"objectType\":\"TelecomAddress\"" +
+                               "},{" +
+                               "\"address\":\"" + personOrgTel + "\"," +
+                               "\"addressType\":\"Telephone\"," +
+                               "\"objectType\":\"TelecomAddress\"" +
+                               "}]";
+        } else if(!"".equals(personOrgEmail)) {
+            telecomAddresses = "[{" +
+                               "\"address\":\"" + personOrgEmail + "\"," +
+                               "\"addressType\":\"Email\"," +
+                               "\"objectType\":\"TelecomAddress\"" +
+                               "}]";
+        } else if(!"".equals(personOrgTel)) {
+            telecomAddresses = "[{" +
+                               "\"address\":\"" + personOrgTel + "\"," +
+                               "\"addressType\":\"Telephone\"," +
+                               "\"objectType\":\"TelecomAddress\"" +
+                               "}]";
+        } else {
+            telecomAddresses = "[]";
+        }
+        return telecomAddresses;
     }
 
 }
