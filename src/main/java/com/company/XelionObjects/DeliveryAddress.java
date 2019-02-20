e2469957262920af619f7a894c11d4bf5f71ecbf
Paul Korver <prkorver@hotmail.com>
2019-02-20 14:01:00 +0100

    Added DeliveryAddress for xelion data model.

---
+++ src/main/java/com/company/XelionObjects/DeliveryAddress.java
@@ -0,0 +1,11 @@
+package com.company.XelionObjects;
+
+public class DeliveryAddress {
+    private String street;
+    private String streetNumber;
+    private String zipCode;
+    private String city;
+    private String state;
+    private String country;
+
+}
